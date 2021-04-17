b3896ff2bf5319904e484556a8d6f8340de70a8a
Md. Solayman <mdsolaymannuhin@gmail.com>
2021-04-18 03:04:26 +0600

    makeAdminFunctioned

--- src/components/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/components/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,43 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const MakeAdmin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    fetch(`http://localhost:5000/makeAdmin`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        alert("Admin Added");
+      });
+  };
+
   return (
-    <div>
-      <h1> I am from Make admin</h1>
+    <div className="w-75 m-5 mx-auto p-5">
+      <h2 className="text-center">Make Admin</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            name="email"
+            {...register("email", { required: true })}
+          />
+          {errors.email && <span>This field is required</span>}
+        </div>
+
+        <input className="btn btn-primary" type="submit" />
+      </form>
     </div>
   );
 };
